34e11fa3c76463d5aa4463faf203f2874b81d2eb
sananbsd2021 <89647979+sananbsd2021@users.noreply.github.com>
2024-03-05 16:57:06 +0700

    Update page.jsx

--- app/dashboard/bookbid/add/page.jsx
+++ app/dashboard/bookbid/add/page.jsx
@@ -1,7 +1,19 @@
-const BookbidAddPage = () => {
+import { addBookbid } from "@/app/lib/actions";
+import styles from "@/app/ui/dashboard/products/addProduct/addProduct.module.css";
+
+const AddBookbidPage = () => {
   return (
-    <div>Bookbid AddPage</div>
+    <div className={styles.container}>
+      <form action={addBookbid} className={styles.form}>
+        <input type="text" placeholder="เลขทะเบียน" name="title" required />
+        <input type="desc" placeholder="เรื่อง" name="desc" required />
+        <input type="desc2" placeholder="หน่วยงาน" name="desc2" required />
+        <input type="desc3" placeholder="ผู้ออก" name="desc3" required />
+        <input type="year" placeholder="ปี พ.ศ." name="year" required />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 };
 
-export default BookbidAddPage;
+export default AddBookbidPage;
