ea05b3c51b916e2fb6af65c4e59e012243a8091e
sananbsd2021 <89647979+sananbsd2021@users.noreply.github.com>
2024-03-05 11:42:54 +0700

    Update page.jsx

--- app/dashboard/sarabans/add/page.jsx
+++ app/dashboard/sarabans/add/page.jsx
@@ -8,7 +8,7 @@ const AddSarabanPage = () => {
         <input type="text" placeholder="เลขทะเบียน" name="title" required />
         <input type="desc" placeholder="เรื่อง" name="desc" required />
         <input type="desc2" placeholder="หน่วยงาน" name="desc2" required />
-        <input type="desc3" placeholder="ผู้รับ" name="desc3" required />
+        <input type="desc3" placeholder="ผู้รับ" name="desc3" />
         {/* <input type="desc4" placeholder="ผู้จ่าย" name="desc4" required /> */}
         <button type="submit">Submit</button>
       </form>
