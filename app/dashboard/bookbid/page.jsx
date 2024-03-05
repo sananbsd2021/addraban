a60c8b3b33905cff8df8c9f8d00cd56666001ffd
sananbsd2021 <89647979+sananbsd2021@users.noreply.github.com>
2024-03-05 16:56:32 +0700

    Update page.jsx

--- app/dashboard/bookbid/page.jsx
+++ app/dashboard/bookbid/page.jsx
@@ -1,7 +1,70 @@
-const BookbidPage = () => {
+import Link from "next/link";
+import styles from "@/app/ui/dashboard/products/products.module.css";
+import Search from "@/app/ui/dashboard/search/search";
+import Pagination from "@/app/ui/dashboard/pagination/pagination";
+import { fetchBookbids } from "@/app/lib/data";
+import { deleteBookbid } from "@/app/lib/actions";
+
+const BookBidPage = async ({ searchParams }) => {
+  const q = searchParams?.q || "";
+  const page = searchParams?.page || 1;
+  const { count, bookbids } = await fetchBookbids(q, page);
+
   return (
-    <div>BookbidcPage</div>
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for a bookbid..." />
+        <Link href="/dashboard/bookbid/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>เลขทะเบียน</td>
+            <td>รายละเอียด</td>
+            <td>หน่วยงาน</td>
+            <td>ผู้ออก</td>
+            <td>ปี พ.ศ.</td>
+            <td>Created At</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          {bookbids.map((bookbid) => (
+            <tr key={bookbid.id}>
+              <td>
+                <div className={styles.product}>
+                  {bookbid.title}
+                </div>
+              </td>
+              <td>{bookbid.desc}</td>
+              <td>{bookbid.desc2}</td>
+              <td>{bookbid.desc3}</td>
+              <td>{bookbid.year}</td>
+              <td>{bookbid.createdAt?.toString().slice(4, 16)}</td>
+              <td>
+                <div className={styles.buttons}>
+                  <Link href={`/dashboard/bookbid/${bookbid.id}`}>
+                    <button className={`${styles.button} ${styles.view}`}>
+                      View
+                    </button>
+                  </Link>
+                  <form action={deleteBookbid}>
+                    <input type="hidden" name="id" value={bookbid.id} />
+                    <button className={`${styles.button} ${styles.delete}`}>
+                      Delete
+                    </button>
+                  </form>
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <Pagination count={count} />
+    </div>
   );
 };
 
-export default BookbidPage;
+export default BookBidPage;
