0f19a0af11b51969bb5323990c81708b5748fc6b
sananbsd2021 <89647979+sananbsd2021@users.noreply.github.com>
2024-03-06 15:32:31 +0700

    Update page.jsx

--- app/dashboard/booksetbids/add/page.jsx
+++ app/dashboard/booksetbids/add/page.jsx
@@ -1,1 +1,27 @@
+import { addBooksetbid } from "@/app/lib/actions";
+import styles from "@/app/ui/dashboard/products/addProduct/addProduct.module.css";
+
+const AddBookacceptPage = () => {
+  return (
+    <div className={styles.container}>
+      <form action={addBooksetbid} className={styles.form}>
+        <input type="text" placeholder="ที่คำสั่ง" name="title" />
+        {/* <input type="desc2" placeholder="ที่หนังสือ" name="desc2" /> */}
+        {/* <input type="desc4" placeholder="จาก" name="desc4" />
+        <input type="desc5" placeholder="ถึง" name="desc5" /> */}
+        <textarea
+          name="desc"
+          id="desc"
+          rows="5"
+          placeholder="เรื่อง"
+        ></textarea>
+        <input type="desc3" placeholder="ลงวันที่" name="desc3" />
+        {/* <input type="desc6" placeholder="หมายเหตุ" name="desc6" /> */}
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddBookacceptPage;
 
