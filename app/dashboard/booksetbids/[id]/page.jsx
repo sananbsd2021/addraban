b3e01c58e795e360385476aee24b1c170ebe8a80
sananbsd2021 <89647979+sananbsd2021@users.noreply.github.com>
2024-03-06 15:33:19 +0700

    Update page.jsx

--- app/dashboard/booksetbids/[id]/page.jsx
+++ app/dashboard/booksetbids/[id]/page.jsx
@@ -1,1 +1,48 @@
+import { updateBookaccept } from "@/app/lib/actions";
+import { fetchBookaccept } from "@/app/lib/data";
+import styles from "@/app/ui/dashboard/products/singleProduct/singleProduct.module.css";
+import Image from "next/image";
+
+const SingleBookacceptPage = async ({ params }) => {
+  const { id } = params;
+  const bookaccept = await fetchBookaccept(id);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.infoContainer}>
+        <div className={styles.imgContainer}>
+          <Image src="/noavatar.png" alt="" fill />
+        </div>
+        {bookaccept.title}
+      </div>
+      <div className={styles.formContainer}>
+        <form action={updateBookaccept} className={styles.form}>
+          <input type="hidden" name="id" value={bookaccept.id} />
+          <label>เลขรับ</label>
+          <input type="text" name="title" placeholder={bookaccept.title} />
+          <label>ที่หนังสือ</label>
+          <input type="text" name="desc2" placeholder={bookaccept.desc2} />
+          <label>ลงวันที่</label>
+          <input type="text" name="desc3" placeholder={bookaccept.desc3} />
+          <label>จาก</label>
+          <input type="text" name="desc4" placeholder={bookaccept.desc4} />
+          <label>ถึง</label>
+          <input type="text" name="desc5" placeholder={bookaccept.desc5} />
+          <label>เรื่อง</label>
+          <textarea
+            name="desc"
+            id="desc"
+            rows="10"
+            placeholder={bookaccept.desc}
+          ></textarea>
+          <label>หมายเหตุ</label>
+          <input type="text" name="descุ" placeholder={bookaccept.descุ} />
+          <button>Update</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SingleBookacceptPage;
 
