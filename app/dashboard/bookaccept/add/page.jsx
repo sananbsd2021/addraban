0e6d0780ed1dad72b16f68b9b892e90ac6c95c77
sananbsd2021 <89647979+sananbsd2021@users.noreply.github.com>
2024-03-07 10:12:04 +0700

    Update page.jsx

--- app/dashboard/bookaccept/add/page.jsx
+++ app/dashboard/bookaccept/add/page.jsx
@@ -5,12 +5,12 @@ const AddBookacceptPage = () => {
   return (
     <div className={styles.container}>
       <form action={addBookaccept} className={styles.form}>
-        <input type="text" placeholder="เลขทะเบียน" name="title" required />
-        <input type="desc" placeholder="เรื่อง" name="desc" required />
-        <input type="desc2" placeholder="หน่วยงาน" name="desc2" required />
-        <input type="desc3" placeholder="ผู้รับ" name="desc3" required />
-        <input type="desc4" placeholder="ผู้จ่าย" name="desc4" required />
-        <input type="year" placeholder="ปี พ.ศ." name="year" required />
+        <input type="text" placeholder="ที่รับ" name="title" required />
+        <input type="desc" placeholder="เรื่อง" name="desc" />
+        <input type="desc2" placeholder="หน่วยงาน" name="desc2" />
+        <input type="desc3" placeholder="ผู้รับ" name="desc3" />
+        <input type="desc4" placeholder="ผู้จ่าย" name="desc4" />
+        <input type="year" placeholder="ปี พ.ศ." name="year" />
         <button type="submit">Submit</button>
       </form>
     </div>
