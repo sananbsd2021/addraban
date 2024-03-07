3989b4105f52252379feaed14f1617bc2c6399e7
sananbsd2021 <89647979+sananbsd2021@users.noreply.github.com>
2024-03-07 10:10:45 +0700

    Update page.jsx

--- app/dashboard/booksends/add/page.jsx
+++ app/dashboard/booksends/add/page.jsx
@@ -5,11 +5,11 @@ const AddBooksendPage = () => {
   return (
     <div className={styles.container}>
       <form action={addBooksend} className={styles.form}>
-        <input type="text" placeholder="เลขทะเบียน" name="title" required />
-        <input type="desc" placeholder="เรื่อง" name="desc" required />
-        <input type="desc2" placeholder="หน่วยงาน" name="desc2" required />
-        <input type="desc3" placeholder="ผู้รับ" name="desc3" required />
-        <input type="desc4" placeholder="ผู้จ่าย" name="desc4" required />
+        <input type="text" placeholder="ที่ส่ง" name="title" required />
+        <input type="desc" placeholder="เรื่อง" name="desc" />
+        <input type="desc2" placeholder="หน่วยงาน" name="desc2" />
+        <input type="desc3" placeholder="ผู้รับ" name="desc3" />
+        <input type="desc4" placeholder="ผู้จ่าย" name="desc4" />
         <input type="year" placeholder="ผู้จ่าย" name="year" />
         <button type="submit">Submit</button>
       </form>
