35753b9dde4c37068e82ff05b696c6e337714d07
sananbsd2021 <89647979+sananbsd2021@users.noreply.github.com>
2024-03-22 13:35:18 +0700

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 const Homepage = () => {
   return (
-    <div className='text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl'>Homepage E-Saraban</div>
+    <div className='text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl'>Book Registration(E-Saraban)</div>
   )
 }
 
