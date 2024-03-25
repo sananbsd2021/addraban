facb8669d13ccbd2d7384ebbd6840b23c7450b87
sananbsd2021 <89647979+sananbsd2021@users.noreply.github.com>
2024-03-25 09:59:38 +0700

    Update page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,5 +1,5 @@
-import BookSendsPage from "@/ui/dashboard/booksends/booksends";
-import BookAcceptPage from "@/ui/dashboard/bookaccepts/bookaccept";
+import BookSendsPage from "../ui/dashboard/booksends/booksends";
+import BookAcceptPage from "../ui/dashboard/bookaccepts/bookaccept";
 import Card from "../ui/dashboard/card/card";
 import Chart from "../ui/dashboard/chart/chart";
 import styles from "../ui/dashboard/dashboard.module.css";
