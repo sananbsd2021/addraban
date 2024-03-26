65616ee40ac4059718b73475bf35946a997ef6ef
sananbsd2021 <89647979+sananbsd2021@users.noreply.github.com>
2024-03-26 11:05:01 +0700

    Update loginForm.jsx

--- app/ui/login/loginForm/loginForm.jsx
+++ app/ui/login/loginForm/loginForm.jsx
@@ -12,7 +12,7 @@ const LoginForm = () => {
       <h1>ดัชนีการสืบค้นข้อมูลด้วยระบบคอมพิวเตอร์</h1>
       <input type="text" placeholder="username" name="username" />
       <input type="password" placeholder="password" name="password" />
-      <button>Login</button>
+      <button>เข้าสู่ระบบ</button>
       {state && state}
     </form>
   );
