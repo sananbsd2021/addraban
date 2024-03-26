7709ed0bf6e4185348edcaf0b4016dc70881ae0e
sananbsd2021 <89647979+sananbsd2021@users.noreply.github.com>
2024-03-26 09:10:33 +0700

    Update page.jsx

--- app/dashboard/sarabans/add/page.jsx
+++ app/dashboard/sarabans/add/page.jsx
@@ -6,10 +6,15 @@ const AddSarabanPage = () => {
     <div className={styles.container}>
       <form action={addSaraban} className={styles.form}>
         <input type="text" placeholder="เลขทะเบียน" name="title" required />
-        <input type="desc" placeholder="เรื่อง" name="desc" required />
-        <input type="desc2" placeholder="หน่วยงาน" name="desc2" required />
-        <input type="year" placeholder="ปี พ.ศ." name="year" />
-        {/* <input type="desc4" placeholder="ผู้จ่าย" name="desc4" required /> */}
+        <textarea
+            name="desc"
+            id="desc"
+            rows="10"
+            placeholder="เรื่อง"
+          ></textarea>
+        <input type="desc2" placeholder="ลงวันที่" name="desc2" required />
+        <input type="desc3" placeholder="หน่วยงาน" name="desc3" />
+        <input type="desc4" placeholder="การปฏิบัติ" name="desc4" required />
         <button type="submit">Submit</button>
       </form>
     </div>
