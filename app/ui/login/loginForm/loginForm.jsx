ba11c3a19b4bea9c65c3365b18e6770023d74cca
sananbsd2021 <89647979+sananbsd2021@users.noreply.github.com>
2024-03-26 11:04:22 +0700

    Update loginForm.jsx

--- app/ui/login/loginForm/loginForm.jsx
+++ app/ui/login/loginForm/loginForm.jsx
@@ -9,7 +9,7 @@ const LoginForm = () => {
 
   return (
     <form action={formAction} className={styles.form}>
-      <h1>Login</h1>
+      <h1>ดัชนีการสืบค้นข้อมูลด้วยระบบคอมพิวเตอร์</h1>
       <input type="text" placeholder="username" name="username" />
       <input type="password" placeholder="password" name="password" />
       <button>Login</button>
