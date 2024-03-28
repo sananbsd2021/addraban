c9058687ca362b8e8be11f225c9e16e0f83cc7ef
sananbsd2021 <89647979+sananbsd2021@users.noreply.github.com>
2024-03-28 13:06:47 +0700

    Update booksends.jsx

--- app/ui/dashboard/booksends/booksends.jsx
+++ app/ui/dashboard/booksends/booksends.jsx
@@ -1,25 +1,27 @@
 import styles from "@/app/ui/dashboard/products/products.module.css";
+import Search from "@/app/ui/dashboard/search/search";
 import Pagination from "@/app/ui/dashboard/pagination/pagination";
 import { fetchBooksends } from "@/app/lib/data";
 
 const BookSendsPage = async ({ searchParams }) => {
-//  const q = searchParams?.q || "";
-//  const page = searchParams?.page || 1;
+  const q = searchParams?.q || "";
+  const page = searchParams?.page || 1;
   const { count, booksends } = await fetchBooksends(q, page);
 
   return (
     <div className={styles.container}>
       <div className={styles.top}>
+        <Search placeholder="Search for a booksends..." />
         <div>หนังสือส่ง</div>
       </div>
       <table className={styles.table}>
         <thead>
           <tr>
-            <td>เลขส่ง</td>
-            <td>เรื่อง</td>
-            <td>ลงวันที่</td>
-            <td>จาก</td>
-            <td>ถึง</td>
+            <td>ทะเบียน</td>
+            <td>รายละเอียด</td>
+            <td>หน่วยงาน</td>
+            <td>ผู้รับ</td>
+            <td>ผู้จ่าย</td>
           </tr>
         </thead>
         <tbody>
@@ -31,9 +33,9 @@ const BookSendsPage = async ({ searchParams }) => {
                 </div>
               </td>
               <td>{booksend.desc}</td>
+              <td>{booksend.desc2}</td>
               <td>{booksend.desc3}</td>
-              <td>{booksend.desc4}</td>
-              <td>{booksend.desc5}</td> 
+              <td>{booksend.desc4}</td> 
             </tr>
           ))}
         </tbody>
